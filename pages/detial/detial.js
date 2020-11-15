10f0614c42d847ccb45da92bab4614f8b89b10c9
unknown <380835979@qq.com>
2020-11-16 00:02:33 +0800

    15日

--- pages/detial/detial.js
+++ pages/detial/detial.js
@@ -5,6 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
+    ensureList:[
+      {
+        enClass:'icon-sousuo',
+        enValue:'七天无理由退货'
+      },
+      {
+        enClass:'icon-sousuo',
+        enValue:'假一赔四'
+      },
+      {
+        enClass:'icon-sousuo',
+        enValue:'官方保障'
+      },
+      {
+        enClass:'icon-sousuo',
+        enValue:'公益宝贝'
+      }
+    ],
     goodsDetial:{
       id:101,
       mainImgUrl:'https://ae01.alicdn.com/kf/Ud46e087860fa40ecaa8f38ae8f84e3fdQ.jpg',
@@ -12,7 +30,45 @@ Page({
       oldPrice: 220,
       tittle: 'b.box三合一多用辅食碗 婴儿吸管碗宝宝星星图案 宝宝吸管碗',
       attribute: 'YEF039 四季款元宝鼠红色，3.78kg1件，可选服务'
-    }
+    },
+    goods:[
+      {
+        tittle:'babycare儿童水杯三合一多用吸管杯',
+        imgurl:'http://img30.360buyimg.com/popWareDetail/jfs/t1/120814/35/8235/101223/5f23b42fE1757e59d/dc4fc720faf68b0a.jpg',
+        price:388,
+        oldPrice:600
+      },
+      {
+        tittle:'babycare儿童水杯三合一多用吸管杯',
+        imgurl:'https://img13.360buyimg.com/imgzone/jfs/t1/124599/26/16300/101100/5f969c94E4cd12b15/ba4735a730c97491.jpg',
+        price:166,
+        oldPrice:530
+      },
+      {
+        tittle:'babycare儿童水杯三合一多用吸管杯',
+        imgurl:'http://img30.360buyimg.com/popWareDetail/jfs/t1/147496/5/10332/204135/5f815356Ee3bdb99e/544dca8fb323e415.jpg',
+        price:88,
+        oldPrice:360
+      },
+      {
+        tittle:'babycare儿童水杯三合一多用吸管杯',
+        imgurl:'http://img30.360buyimg.com/popWareDetail/jfs/t1/120814/35/8235/101223/5f23b42fE1757e59d/dc4fc720faf68b0a.jpg',
+        price:388,
+        oldPrice:600
+      },
+      {
+        tittle:'babycare儿童水杯三合一多用吸管杯',
+        imgurl:'https://img13.360buyimg.com/imgzone/jfs/t1/124599/26/16300/101100/5f969c94E4cd12b15/ba4735a730c97491.jpg',
+        price:166,
+        oldPrice:530
+      },
+      {
+        tittle:'babycare儿童水杯三合一多用吸管杯',
+        imgurl:'http://img30.360buyimg.com/popWareDetail/jfs/t1/147496/5/10332/204135/5f815356Ee3bdb99e/544dca8fb323e415.jpg',
+        price:88,
+        oldPrice:360
+      }
+    ]
   },
 
   /**
