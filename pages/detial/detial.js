89abdf3082024a96875e6fd6e014ce10b6df8831
380835979@qq.com <longchun@ctf.com.cn>
2020-11-19 18:28:29 +0800

    19

--- pages/detial/detial.js
+++ pages/detial/detial.js
@@ -7,7 +7,11 @@ Page({
   data: {
     goodsDetial:{
       id:101,
-      mainImgUrl:'https://ae01.alicdn.com/kf/Ud46e087860fa40ecaa8f38ae8f84e3fdQ.jpg',
+      mainImgUrl:[
+        'http://img30.360buyimg.com/popWareDetail/jfs/t1/120814/35/8235/101223/5f23b42fE1757e59d/dc4fc720faf68b0a.jpg',
+        'https://img13.360buyimg.com/imgzone/jfs/t1/124599/26/16300/101100/5f969c94E4cd12b15/ba4735a730c97491.jpg',
+        'http://img30.360buyimg.com/popWareDetail/jfs/t1/147496/5/10332/204135/5f815356Ee3bdb99e/544dca8fb323e415.jpg'
+      ],
       price: 180,
       oldPrice: 220,
       tittle: 'b.box三合一多用辅食碗 婴儿吸管碗宝宝星星图案 宝宝吸管碗',
