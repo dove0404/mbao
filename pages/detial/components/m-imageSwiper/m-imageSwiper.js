a4ae69e2c6efb060fdbd0ce9c280ac4bbe077413
380835979@qq.com <longchun@ctf.com.cn>
2020-11-20 17:46:46 +0800

    20

--- pages/detial/components/m-imageSwiper/m-imageSwiper.js
+++ pages/detial/components/m-imageSwiper/m-imageSwiper.js
@@ -21,6 +21,12 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    showPreviewImage(e) {
+      const current = e.currentTarget.dataset.url //取出传进来的url
+      wx.previewImage({
+        current: current, // 当前显示图片的http链接
+        urls: this.data.bigImgList // 需要预览的图片http链接列表
+      })
+    }
   }
 })
