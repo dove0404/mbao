4d9cde282c253e80d5db12147382c0c6dcc1c3a9
unknown <380835979@qq.com>
2020-12-02 23:16:52 +0800

    12.2

--- pages/goodList/goodList.js
+++ pages/goodList/goodList.js
@@ -2,32 +2,6 @@
 import{ getGoodListData } from '../../service/goodList'
 Page({
   data: {
-    brandList:[
-      {
-        value:'童泰',
-        imgurl:'https://ae01.alicdn.com/kf/U80bd58845494408c84467a4aed5bcfa0K.jpg'
-      },
-      {
-        value:'HM',
-        imgurl:'https://ae01.alicdn.com/kf/U3f487da37e08400a9de381c65d143c076.jpg'
-      },
-      {
-        value:'巴拉巴拉',
-        imgurl:'https://ae01.alicdn.com/kf/U0d22e702fd6a4c3d895aca37a1e479c4D.jpg'
-      },
-      {
-        value:'purebom',
-        imgurl:'https://ae01.alicdn.com/kf/U91c03f27ed89432b84c903241efe3b4cr.jpg'
-      },
-      {
-        value:'巴拉巴拉',
-        imgurl:'https://ae01.alicdn.com/kf/U0d22e702fd6a4c3d895aca37a1e479c4D.jpg'
-      },
-      {
-        value:'purebom',
-        imgurl:'https://ae01.alicdn.com/kf/U91c03f27ed89432b84c903241efe3b4cr.jpg'
-      }
-    ],
     goods:[]
   },
 
@@ -35,9 +9,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options)
     // 直接调用下面封装号的函数即可
     this._getGoodListData()
   },
+  // ----------------------------网络请求函数----------------------------------
   // 将请求结果封装成_getGoodListData函数，上面直接调用即可
   _getGoodListData() {
     getGoodListData().then(res =>{
